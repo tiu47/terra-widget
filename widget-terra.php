a637347976a1eedc24025f6aeff0c1a3159cf47b
tiu47 <soy@arielbarrios.com>
2021-09-20 13:58:42 -0300

    ss

--- widget-terra.php
+++ widget-terra.php
@@ -55,29 +55,24 @@ class My_Widget extends WP_Widget {
 
 
 		<div class="container my-5">
-			<div class="row">
-				<div class="col">
-					<form class="row g-3">
-						<div class="col-12">
-							<label for="inputState" class="form-label">Select Currency Pair</label>
+					<div class="row ">
+						<div class="col d-flex  align-items-center">
+							<label for="inputState" class="form-label col-3">Select Currency Pair</label>
 							<select onchange="submitcurrency(this.value,5)" id="inputState" class="form-select">
-								<option value="" ></option>
-								<option value="USD/CAD" >USD/CAD</option>
+								<option value="USD/CAD" selected>USD/CAD</option>
 								<option value="USD/EUR" >USD/EUR</option>
 								<option value="USD/AUD" >USD/AUD</option>
 								<option value="EUR/AUD" >EUR/AUD</option>
 								<option value="EUR/JPY" >EUR/JPY</option>
 							</select>
 						</div>
-				</form>
-
 				</div>
-			</div>
 		</div>
 
 
+
 		<div class="container my-5">
-			<div class="row text-center  align-items-center align-space-between justify-content-center border py-3 rounded">
+			<div class="row text-center  align-items-center align-space-between justify-content-center border py-3 rounded  bg-light">
 				<div class="col-5 "><h2 class="baseCurrecncy-class">Base Currecncy</h2></div>
 				<div class="col-1  display-6"> = </div>
 				<div class="col-5 "><h2 class="quoteCurrency-class">Quote Currency</h2></div>
@@ -86,42 +81,47 @@ class My_Widget extends WP_Widget {
 
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js" integrity="sha256-bC3LCZCwKeehY6T4fFi9VfOU0gztUa+S4cnkIhVPZ5E=" crossorigin="anonymous"></script>
+
 
 		<script>
 
 
 		$(document).ready(function() {
 
-			// function checkStatus( ) {
-			//
-			//     function check( ) {
-			//         //Check with server. Should I set status to inactive
-			//
-			//         var timer = setTimeout(check, 60 * 1000);
-			//     }
-			//     check();
-			// }
+			submitcurrency($('#inputState').val(),5)
+      		var timer = setInterval(function(){ submitcurrency($('#inputState').val(),5) }, 1000 * 60 * 60);
+
+
 
 		})
+
+
+		function active_menu(days) {
+
+				$('.button_group .btn').removeClass('btn-secondary');
+
+				$("button[data-days='" + days +"']").addClass('btn-secondary');
+				// $($('.button_group .btn').attr('data-days')).addClass('btn-secondary');
+		}
 
 		</script>
 
 
-		<script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js" integrity="sha256-bC3LCZCwKeehY6T4fFi9VfOU0gztUa+S4cnkIhVPZ5E=" crossorigin="anonymous"></script>
 
 
 		<div class="container my-5">
-			<div class="row">
-				<div class="col">
-					<button type="button"  onclick="submitcurrency($('#inputState').val(),1)" class="btn  btn-outline-secondary  btn-sm">Day</button>
-					<button type="button" onclick="submitcurrency($('#inputState').val(),5)" class="btn  btn-secondary  btn-sm">5 Days</button>
-					<button type="button"  onclick="submitcurrency($('#inputState').val(),8)" class="btn  btn-outline-secondary  btn-sm">8 Days</button>
-
+			<div class="row justify-content-center">
+				<div class="col-6 my-0" >
+					<canvas id="myChart" width="400" height="400"></canvas>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col">
-					<canvas id="myChart" width="400" height="400"></canvas>
+			<div class="row  ">
+				<div class="col button_group d-flex  align-space-between justify-content-center">
+					<button type="button"  onclick="submitcurrency($('#inputState').val(),1);active_menu(1)" data-days="1" class="btn border-0  btn-outline-secondary  btn-sm mx-1">Day</button>
+					<button type="button" onclick="submitcurrency($('#inputState').val(),5);active_menu(5)" data-days="5" class="btn  border-0 btn-secondary  btn-sm mx-1">5 Days</button>
+					<button type="button"  onclick="submitcurrency($('#inputState').val(),8);active_menu(8)" data-days="8" class="btn  border-0 btn-outline-secondary  btn-sm mx-1 ">8 Days</button>
+
 				</div>
 			</div>
 
@@ -135,54 +135,54 @@ class My_Widget extends WP_Widget {
 		</div>
 
 
+
+
+
+
+
+
+
+
 		<script>
 
-		// const actions = [
-		// 	{
-		// 		name: 'Randomize',
-		// 		handler(chart) {
-		// 			chart.data.datasets.forEach(dataset => {
-		// 				dataset.data = Utils.numbers({count: chart.data.labels.length, min: -100, max: 100});
-		// 			});
-		// 			chart.update();
-		// 		}
-		// 	}
-		// ];
+			const theapiKey="89225ded3fb447d5ef3e";
+
+			let labels = [];
+
+			let dataChart = {
+			  labels: labels,
+			  datasets: [{
+			    label: '',
+			    backgroundColor: 'rgb(255, 99, 132)',
+			    borderColor: 'rgb(255, 99, 132)',
+			    data: [],
+			},
+
+			{
+			  label: '',
+			  backgroundColor: 'rgb(99, 101, 255)',
+			  borderColor: 'rgb(107, 99, 255)',
+			  data: [],
+			}
+
+			]};
+
+			const config = {
+			  type: 'bar',
+			  data: dataChart,
+			  options: {}
+			};
+
+			// === include 'setup' then 'config' above ===
+			var myChart = new Chart(
+			  document.getElementById('myChart'),
+			  config
+			);
 
 
 
-		const theapiKey="89225ded3fb447d5ef3e";
-
-		let labels = [];
-
-		let dataChart = {
-		  labels: labels,
-		  datasets: [{
-		    label: 'My First dataset',
-		    backgroundColor: 'rgb(255, 99, 132)',
-		    borderColor: 'rgb(255, 99, 132)',
-		    data: [],
-		  }]
-		};
-
-		const config = {
-		  type: 'line',
-		  data: dataChart,
-		  options: {}
-		};
-
-		// === include 'setup' then 'config' above ===
-
-		var myChart = new Chart(
-		  document.getElementById('myChart'),
-		  config
-		);
 
 
-		</script>
-
-
-		<script type="text/javascript">
 
 		var tehatavar;
 
@@ -210,11 +210,11 @@ class My_Widget extends WP_Widget {
 		let actualdate =  get_year(0) +'-'+get_month(0)+'-'+get_day(0);
 		let actualdate_minus2 =  get_year(0) +'-'+get_month(2)+'-'+get_day(0);
 		let daterequested_minus2 =  get_year(0) +'-'+get_month(2)+'-'+get_day(8);
-		// let actualdate_minus2 =  get_year() +'-'+get_month(2)+'-'+get_day();
 
 
 		function submitcurrency(currenccyPair,timePeriod){
 
+			active_menu(timePeriod);
 			timePeriodDays =  get_year(0) +'-'+get_month(2)+'-'+get_day(timePeriod);
 			currenrrancyArray = currenccyPair.split("/") ;
 
@@ -227,79 +227,55 @@ class My_Widget extends WP_Widget {
 
 			$.get( "https://free.currconv.com/api/v7/convert?q="+baseCurrecncy+"_"+quoteCurrency+","+quoteCurrency+"_"+baseCurrecncy+"&compact=ultra&apiKey="+theapiKey+"", function( data ) {
 
-			// https://free.currconv.com/api/v7/convert?apiKey=do-not-use-this-api-key-amXyxtfNjkpe1Hjzdafc_&q=USD_PHP,PHP_USD&compact=ultra&date=2021-06-11&endDate=2021-06-16
-
-			// $.get( "https://api.currconv.com/api/v7/convert?q="+baseCurrecncy+"_"+quoteCurrency+"&compact=ultra&apiKey="+theapiKey+"", function( data ) {
-			// /api/v7/convert?q=USD_PHP,PHP_USD&compact=ultra&apiKey=[YOUR_API_KEY]
-
 				tehatavar = data;
 				let baseCurrecncy_quoteCurrency = baseCurrecncy+'_'+quoteCurrency;
 				let quoteCurrency_baseCurrecncy = quoteCurrency+'_'+baseCurrecncy;
 			    console.log(tehatavar[baseCurrecncy+'_'+quoteCurrency]);
 				$(".baseCurrecncy-class").text(baseCurrecncy+': '+tehatavar[baseCurrecncy_quoteCurrency]);
 				$(".quoteCurrency-class").text(quoteCurrency+': '+tehatavar[quoteCurrency_baseCurrecncy]);
-
-
-
-
-
-				// $(".timeStamp").text(new Date($.now()));
 				$(".timeStamp").text(new Date($.now()));
-
-
-				// dataChart.labels =[baseCurrecncy,quoteCurrency];
-				// dataChart.datasets[0].data =[tehatavar[baseCurrecncy+'_'+quoteCurrency] , tehatavar[quoteCurrency+'_'+baseCurrecncy]];
-				// dataChart.datasets[0].label =[tehatavar[baseCurrecncy+'_'+quoteCurrency] , tehatavar[quoteCurrency+'_'+baseCurrecncy]];
-				// console.log(dataChart);
-				// myChart.update();
-
 
 			});
 
-			// date
-			// const restore = () => style.innerHTML = '';
 
 
-			$.get("https://free.currconv.com/api/v7/convert?apiKey="+theapiKey+"&q="+baseCurrecncy+"_"+quoteCurrency+","+quoteCurrency+"_"+baseCurrecncy+"&compact=ultra&date="+timePeriodDays+"&endDate="+actualdate_minus2, function( data ) {
+			$.get("https://free.currconv.com/api/v7/convert?apiKey="+theapiKey+"&q="+baseCurrecncy+"_"+quoteCurrency+","+quoteCurrency+"_"+baseCurrecncy+"&compact=ultra&date="+timePeriodDays+"&endDate="+actualdate_minus2, function(data) {
 				timerangedata = data;
-				// aaread = baseCurrecncy+'_'+quoteCurrency;
-			    // console.log(timerangedata[baseCurrecncy+'_'+quoteCurrency]['2021-06-11']);
 			    console.log(timePeriodDays);
 			    console.log(actualdate_minus2);
 			    console.log("https://free.currconv.com/api/v7/convert?apiKey="+theapiKey+"&q="+baseCurrecncy+"_"+quoteCurrency+","+quoteCurrency+"_"+baseCurrecncy+"&compact=ultra&date="+timePeriodDays+"&endDate="+actualdate_minus2);
 
-
-				// $(".baseCurrecncy-class").text(baseCurrecncy+': '+timerangedata[baseCurrecncy+'_'+quoteCurrency]);
-				// $(".quoteCurrency-class").text(quoteCurrency+': '+timerangedata[quoteCurrency+'_'+baseCurrecncy]);
-
 				let days_amount = timePeriod; //i setup five days , but should be a dynamic setup
 				let starting_day = get_day(timePeriod); // again but should be a dynamic setup
 
-				let currency_label_by_day =[];
-				let currency_val_by_day =[];
+				let currency_label_by_day_baseCurrecncy =[];
+				let currency_val_by_day_baseCurrecncy =[];
+
+
+				let currency_label_by_day_quoteCurrency =[];
+				let currency_val_by_day_quoteCurrency =[];
 
 					for (var i = 0; i < days_amount; i++) {
+						currency_val_by_day_baseCurrecncy.push(timerangedata[baseCurrecncy+'_'+quoteCurrency]['2021-06-'+(starting_day + i)]);
+						currency_val_by_day_quoteCurrency.push(timerangedata[quoteCurrency+'_'+baseCurrecncy]['2021-06-'+(starting_day + i)]);
+						currency_label_by_day_baseCurrecncy.push('2021-06-'+(starting_day + i)) ;
+						// currency_label_by_quoteCurrency.push('2021-06-'+(starting_day + i)) ;
 
-						currency_val_by_day.push(timerangedata[baseCurrecncy+'_'+quoteCurrency]['2021-06-'+(starting_day + i)]);
-						currency_label_by_day.push('2021-06-'+(starting_day + i)) ;
-						dataChart.labels = currency_label_by_day ;
-						dataChart.datasets[0].data=currency_val_by_day;
+						dataChart.labels = currency_label_by_day_baseCurrecncy ;
+						dataChart.datasets[0].data=currency_val_by_day_baseCurrecncy;
+						dataChart.datasets[1].data=currency_val_by_day_quoteCurrency;
 						dataChart.datasets[0].label =baseCurrecncy+'/'+quoteCurrency;
 					}
 
 				console.log(dataChart.labels);
 				myChart.update();
 			});
-
-
-
 		}
 
-
- 
-
-
 		</script>
+
+
+
 
 
 		<?php
@@ -309,6 +285,21 @@ class My_Widget extends WP_Widget {
         echo $args['after_widget'];
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public function form( $instance ) {
 
